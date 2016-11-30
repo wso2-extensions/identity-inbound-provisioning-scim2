6c1ff5c7cb956cdc246090d4e06fcfd4eeba509e
Vindula Jayawardana <vindulamj@gmail.com>
2016-11-30 19:53:31 +0530

    Add C5 Based SCIM API implementation

---
+++ components/org.wso2.carbon.identity.inbound.provisioning.scim2.common/src/main/java/org/wso2/carbon/identity/inbound/provisioning/scim2/common/utils/SimpleClaimResolver.java
@@ -0,0 +1,90 @@
+package org.wso2.carbon.identity.inbound.provisioning.scim2.common.utils;
+
+import org.wso2.carbon.identity.mgt.claim.Claim;
+import org.wso2.carbon.identity.mgt.exception.ClaimManagerException;
+import org.wso2.carbon.identity.mgt.exception.IdentityStoreException;
+import org.wso2.carbon.identity.mgt.model.UserModel;
+import org.wso2.charon.core.v2.exceptions.BadRequestException;
+import org.wso2.charon.core.v2.exceptions.CharonException;
+import org.wso2.charon.core.v2.exceptions.NotFoundException;
+import org.wso2.charon.core.v2.objects.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * This class is to mock the claim mapping need to be happened between user core and the SCIM API layer.
+ */
+
+//TODO : This class need to be removed after having the official claim resolver.
+public class SimpleClaimResolver {
+
+    /*
+     * This method is to get the demo user from the user core.
+     * @param userStoreUser
+     * @param claimURIList
+     * @return
+     * @throws CharonException
+     */
+    public static User demoeGetScimUser(org.wso2.carbon.identity.mgt.bean.User userStoreUser,
+                                        List<Claim> claimURIList) throws CharonException {
+        try {
+            User scimUser = null;
+
+            List<String> claimURIs = new ArrayList<>();
+            for (Claim claim : claimURIList) {
+                claimURIs.add(claim.getClaimUri());
+            }
+
+            //obtain user claim values
+            List<Claim> attributes = userStoreUser.getClaims(claimURIs);
+            Map<String, String> attributeMap = new HashMap<>();
+
+            for (Claim claim : attributes) {
+                if (claim.getClaimUri().equals("http://wso2.org/claims/username")) {
+                    attributeMap.put("urn:ietf:params:scim:schemas:core:2.0:User:userName", claim.getValue());
+                }
+            }
+
+            //construct the SCIM Object from the attributes
+            scimUser = (User) SCIMClaimResolver.constructSCIMObjectFromAttributes(attributeMap, 1);
+            //set the schemas of the scim user
+            scimUser.setSchemas();
+
+            return scimUser;
+        } catch (BadRequestException | IdentityStoreException | CharonException |
+                ClaimManagerException | NotFoundException e) {
+            throw new CharonException("Error in getting the user.");
+        }
+    }
+
+    /*
+     * This method is to create the demo user model from claims and their values.
+     * @param claims
+     * @return
+     */
+    public static UserModel demoGetUserModelFromClaims(Map<String, String> claims) {
+        UserModel userModel = new UserModel();
+        List<Claim> claimList = new ArrayList<>();
+        for (Map.Entry<String, String> claim : claims.entrySet()) {
+            if (claim.getKey().equals("urn:ietf:params:scim:schemas:core:2.0:User:userName")) {
+                Claim newClaim = new Claim();
+                newClaim.setClaimUri("http://wso2.org/claims/username");
+                newClaim.setValue(claim.getValue());
+                newClaim.setDialectUri("http://wso2.org/claims");
+                claimList.add(newClaim);
+
+            } else if (claim.getKey().equals("urn:ietf:params:scim:schemas:core:2.0:User:name.givenName")) {
+                Claim newClaim = new Claim();
+                newClaim.setClaimUri("http://wso2.org/claims/firstName");
+                newClaim.setValue(claim.getValue());
+                newClaim.setDialectUri("http://wso2.org/claims");
+                claimList.add(newClaim);
+            }
+        }
+        userModel.setClaims(claimList);
+        return userModel;
+    }
+}
---
+++ components/org.wso2.carbon.identity.inbound.provisioning.scim2.provider/src/main/java/org/wso2/carbon/identity/inbound/provisioning/scim2/provider/mappers/FormatNotSupportedMapper.java
@@ -0,0 +1,27 @@
+package org.wso2.carbon.identity.inbound.provisioning.scim2.provider.mappers;
+
+import org.osgi.service.component.annotations.Component;
+import org.wso2.charon.core.v2.exceptions.FormatNotSupportedException;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+/**
+ * Exception mapper for Charon Exception.
+ */
+
+@Component(
+        name = "org.wso2.carbon.identity.inbound.provisioning.scim2.common.mappers.FormatNotSupportedException;",
+        service = ExceptionMapper.class,
+        immediate = true
+)
+public class FormatNotSupportedMapper implements ExceptionMapper<FormatNotSupportedException> {
+
+    @Override
+    public Response toResponse(FormatNotSupportedException e) {
+        return Response.status(e.getStatus()).
+                entity(e.getDetail()).
+                type("text/plain").
+                build();
+    }
+}
