ecf106ebe5295067dd791bbd4a0afdff97704ceb
Gayan <gmgunawardana@gmail.com>
2017-02-17 17:59:00 +0530

    Fixing IDENTITY-5714

--- components/org.wso2.carbon.identity.inbound.provisioning.scim2.common/src/main/java/org/wso2/carbon/identity/inbound/provisioning/scim2/common/impl/CarbonUserManager.java
+++ components/org.wso2.carbon.identity.inbound.provisioning.scim2.common/src/main/java/org/wso2/carbon/identity/inbound/provisioning/scim2/common/impl/CarbonUserManager.java
@@ -48,13 +48,13 @@ import org.wso2.charon3.core.utils.codeutils.ExpressionNode;
 import org.wso2.charon3.core.utils.codeutils.Node;
 import org.wso2.charon3.core.utils.codeutils.SearchRequest;
 
-import javax.security.auth.callback.Callback;
-import javax.security.auth.callback.PasswordCallback;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.PasswordCallback;
 
 /**
  * This class is to deal with the carbon user core API. This uses identityStore defined API for
@@ -82,8 +82,6 @@ public class CarbonUserManager implements UserManager {
 
             String userStoreDomain = SCIMCommonUtils.extractDomainFromName(user.getUserName(), identityStore);
             user.setUserName(SCIMCommonUtils.removeDomainFromName(user.getUserName()));
-            char[] password = ((SimpleAttribute) (user.getAttribute(SCIMConstants.UserSchemaConstants.PASSWORD))).
-                    getStringValue().toCharArray();
 
             //get the groups attribute as we are going to explicitly store the info of the user's groups
             MultiValuedAttribute groupsAttribute = (MultiValuedAttribute) (
@@ -100,14 +98,20 @@ public class CarbonUserManager implements UserManager {
                 throw new ConflictException("User with the name: " + user.getUserName() +
                         " already exists in the system.");
             }*/
+            Attribute passwordAttribute = user.getAttribute(SCIMConstants.UserSchemaConstants.PASSWORD);
+            if (passwordAttribute != null) {
 
-            PasswordCallback passwordCallback = new PasswordCallback(SCIMConstants.UserSchemaConstants.PASSWORD, false);
-            passwordCallback.setPassword(password);
-            List<Callback> callbackList = new ArrayList<>();
-            callbackList.add(passwordCallback);
-            userBean.setCredentials(callbackList);
+                char[] password = ((SimpleAttribute) (user.getAttribute(SCIMConstants.UserSchemaConstants.PASSWORD))).
+                        getStringValue().toCharArray();
+
+                PasswordCallback passwordCallback = new PasswordCallback(SCIMConstants.UserSchemaConstants.PASSWORD,
+                        false);
+                passwordCallback.setPassword(password);
+                List<Callback> callbackList = new ArrayList<>();
+                callbackList.add(passwordCallback);
+                userBean.setCredentials(callbackList);
+            }
             org.wso2.carbon.identity.mgt.User userStoreUser = identityStore.addUser(userBean, userStoreDomain);
-
             // list to store the group ids which will be used to create the group attribute in scim user.
             List<String> groupIds = new ArrayList<>();
 
