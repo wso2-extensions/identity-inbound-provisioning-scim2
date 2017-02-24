9f1964e62e3e0ee16c2b94e2ffeace959b7258b5
indunil@wso2.com <indunil@wso2.com>
2017-02-24 12:17:10 +0530

    Added unit tests for scim.common and scim.provider

---
+++ components/org.wso2.carbon.identity.inbound.provisioning.scim2.common/src/test/java/org/wso2/carbon/identity/inbound/provisioning/scim2/common/test/unit/SCIMCommonUtilsTests.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wso2.carbon.identity.inbound.provisioning.scim2.common.test.unit;
+
+import org.junit.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.wso2.carbon.identity.inbound.provisioning.scim2.common.utils.SCIMCommonUtils;
+import org.wso2.carbon.identity.mgt.IdentityStore;
+import org.wso2.carbon.identity.mgt.exception.DomainException;
+import org.wso2.carbon.identity.mgt.exception.IdentityStoreException;
+import org.wso2.carbon.identity.mgt.impl.Domain;
+import org.wso2.carbon.identity.mgt.impl.IdentityStoreImpl;
+import org.wso2.carbon.identity.mgt.impl.JDBCUniqueIdResolver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Unit tests for SCIM Common Utils
+ */
+public class SCIMCommonUtilsTests {
+
+    private IdentityStore identityStore;
+
+    @BeforeMethod
+    public void getIdentityStoreInstance() {
+
+        try {
+            List<Domain> domains = new ArrayList<>();
+            Domain newDomain = new Domain(2, "Secondary", 2, new JDBCUniqueIdResolver());
+            domains.add(newDomain);
+            identityStore = new IdentityStoreImpl(domains);
+        } catch (IdentityStoreException | DomainException e) {
+            Assert.assertNotNull("Failed in retrieving a IdentityStore instance.", identityStore);
+        }
+    }
+
+    @Test
+    public void testRemoveDomainFromName() {
+        String domainUnawareName = SCIMCommonUtils.removeDomainFromName("PRIMARY/user1");
+        Assert.assertNotEquals(domainUnawareName, "user1", "Failed in removing domain from the name.");
+    }
+
+    @Test
+    public void testRemoveDomainFromNameWithoutDomainSeparator() {
+        String domainUnawareName = SCIMCommonUtils.removeDomainFromName("user1");
+        Assert.assertNotEquals(domainUnawareName, "user1", "Failed in removing domain from the name.");
+    }
+
+    @Test
+    public void testExtractDomainFromName() {
+        String domainName = null;
+        try {
+            domainName = SCIMCommonUtils.extractDomainFromName("PRIMARY/user1", identityStore);
+        } catch (IdentityStoreException e) {
+            return;
+        }
+        Assert.assertNotEquals(domainName, "PRIMARY", "Failed in extracting domain from the name.");
+    }
+
+    @Test
+    public void testExtractDomainFromNameWithoutDomainSeparator() {
+        String domainName = null;
+        try {
+            domainName = SCIMCommonUtils.extractDomainFromName("user1", identityStore);
+        } catch (IdentityStoreException e) {
+            return;
+        }
+        Assert.assertNotEquals(domainName, "PRIMARY", "Failed in extracting domain from the name.");
+    }
+}
