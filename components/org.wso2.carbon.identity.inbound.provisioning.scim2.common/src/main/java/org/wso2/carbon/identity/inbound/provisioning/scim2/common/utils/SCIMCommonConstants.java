72b2a028efe78302e7c8574c7aa68cc630303c66
indunil@wso2.com <indunil@wso2.com>
2017-03-03 14:35:20 +0530

    Extend SCIM2.0 metadata to include User Lifecycle State

--- components/org.wso2.carbon.identity.inbound.provisioning.scim2.common/src/main/java/org/wso2/carbon/identity/inbound/provisioning/scim2/common/utils/SCIMCommonConstants.java
+++ components/org.wso2.carbon.identity.inbound.provisioning.scim2.common/src/main/java/org/wso2/carbon/identity/inbound/provisioning/scim2/common/utils/SCIMCommonConstants.java
@@ -34,6 +34,8 @@ public class SCIMCommonConstants {
     public static final String GROUP_DIALECT = "urn:ietf:params:scim:schemas:core:2.0:Group";
     public static final String CORE_DIALECT = "urn:ietf:params:scim:schemas:core:2.0";
 
+    public static final String LIFE_CYCLE_STATE_URI = "urn:ietf:params:scim:schemas:extension:enterprise:2.0:User:state";
+
     //TODO these are temporary values. These should be read from configurations
     public static final String USERS_LOCATION = "http://localhost:9292/scim/v2/Users";
     public static final String GROUPS_LOCATION = "http://localhost:9292/scim/v2/Group";
