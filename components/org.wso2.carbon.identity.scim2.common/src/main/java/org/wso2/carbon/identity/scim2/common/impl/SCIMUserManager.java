5e14a8a0a4394f8acf58c0d4c4355ff6063b9458
Isuranga Perera <isuranga@wso2.com>
2019-02-11 14:50:41 +0530

    Fix #4285:Returns 500 If invalid password is provided when creating a user in SCIM2

--- components/org.wso2.carbon.identity.scim2.common/src/main/java/org/wso2/carbon/identity/scim2/common/impl/SCIMUserManager.java
+++ components/org.wso2.carbon.identity.scim2.common/src/main/java/org/wso2/carbon/identity/scim2/common/impl/SCIMUserManager.java
@@ -84,6 +84,7 @@ public class SCIMUserManager implements UserManager {
     public static final String FILTERING_DELIMITER = "*";
     public static final String SQL_FILTERING_DELIMITER = "%";
     private static final String ERROR_CODE_INVALID_USERNAME = "31301";
+    private static final String ERROR_CODE_INVALID_CREDENTIAL = "30003";
     private static Log log = LogFactory.getLog(SCIMUserManager.class);
     private UserStoreManager carbonUM = null;
     private ClaimManager carbonClaimManager = null;
@@ -176,6 +177,9 @@ public class SCIMUserManager implements UserManager {
         while (e != null && i < 10) {
 
             if (e instanceof UserStoreException && e.getMessage().contains(ERROR_CODE_INVALID_USERNAME)) {
+                throw new BadRequestException(e.getMessage(), ResponseCodeConstants.INVALID_VALUE);
+            }
+            if (e instanceof UserStoreException && e.getMessage().contains(ERROR_CODE_INVALID_CREDENTIAL)) {
                 throw new BadRequestException(e.getMessage(), ResponseCodeConstants.INVALID_VALUE);
             }
             if (e instanceof PolicyViolationException) {
