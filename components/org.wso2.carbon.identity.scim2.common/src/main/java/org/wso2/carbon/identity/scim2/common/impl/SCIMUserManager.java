cc79a63e1512b3c0dde99a68966a025a13cd8870
sadilchamishka <sandilchamishka@gmail.com>
2024-02-10 16:21:51 +0530

    Address review comments

--- components/org.wso2.carbon.identity.scim2.common/src/main/java/org/wso2/carbon/identity/scim2/common/impl/SCIMUserManager.java
+++ components/org.wso2.carbon.identity.scim2.common/src/main/java/org/wso2/carbon/identity/scim2/common/impl/SCIMUserManager.java
@@ -1405,7 +1405,7 @@ public class SCIMUserManager implements UserManager {
         if (isFilteringNotSupported(node.getOperation())) {
             String errorMessage =
                     "Filter operation: " + node.getOperation() + " is not supported for filtering in users endpoint.";
-            throw new BadRequestException(errorMessage);
+            throw new BadRequestException(errorMessage, ResponseCodeConstants.INVALID_FILTER);
         }
         domainName = resolveDomainName(domainName, node);
         int totalResults = 0;
@@ -3111,7 +3111,7 @@ public class SCIMUserManager implements UserManager {
         // Check whether the filter operation is supported for filtering in groups.
         if (isFilteringNotSupported(filterOperation)) {
             String errorMessage = "Filter operation: " + filterOperation + " is not supported for groups filtering.";
-            throw new BadRequestException(errorMessage);
+            throw new BadRequestException(errorMessage, ResponseCodeConstants.INVALID_FILTER);
         }
         // Resolve the domain name in request according to 'FilterUsersAndGroupsOnlyFromPrimaryDomain' or
         // EnableFilteringEnhancements' properties in identity.xml or domain name embedded in the filter attribute
